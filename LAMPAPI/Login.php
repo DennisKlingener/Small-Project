096e6a7b8369089907cac56eb7bf47af1ce7181f
willie6646 <140930952+willie6646@users.noreply.github.com>
2024-01-23 19:08:28 -0500

    Update Login.php

--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -7,7 +7,7 @@
 	$firstName = "";
 	$lastName = "";
 
-	$conn = new mysqli("localhost", "TheApi", "1verygoodPassword", "COP4331"); 	
+	$conn = new mysqli("localhost", "TheApi", "1verygoodPassword", "COP4331");		
 	if( $conn->connect_error )
 	{
 		returnWithError( $conn->connect_error );
