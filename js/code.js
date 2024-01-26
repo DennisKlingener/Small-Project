9ca08f2e9c4c90ab7408c436c50b98a1222fdeba
giullyasantos <123971723+giullyasantos@users.noreply.github.com>
2024-01-26 07:57:41 -0500

    Update code.js

--- js/code.js
+++ js/code.js
@@ -1,335 +1,452 @@
-const urlBase = 'http://techdeckers.xyz/LAMPAPI';
-const extension = 'php';
-
-let userId = 0;
-let firstName = "";
-let lastName = "";
-
-function doLogin()
-{
-        userId = 0;
-        firstName = "";
-        lastName = "";
-
-        let login = document.getElementById("loginName").value;
-        let password = document.getElementById("loginPassword").value;
-//      var hash = md5( password );
-
-        //document.getElementById("loginResult").innerHTML = "";
-
-        let tmp = {login:login,password:password};
-//      var tmp = {login:login,password:hash};
-        let jsonPayload = JSON.stringify( tmp );
-
-        let url = urlBase + '/Login.' + extension;
-
-        let xhr = new XMLHttpRequest();
-        xhr.open("POST", url, true);
-        xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-        try
-        {
-                xhr.onreadystatechange = function()
-                {
-                        if (this.readyState == 4 && this.status == 200)
-                        {
-                                let jsonObject = JSON.parse( xhr.responseText );
-                                userId = jsonObject.id;
-
-                                if( userId < 1 )
-                                {
-                                        document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
-                                        return;
-                                }
-
-                                firstName = jsonObject.firstName;
-                                lastName = jsonObject.lastName;
-
-                                saveCookie();
-
-                                window.location.href = "contacts.html";
-                        }
-                };
-                xhr.send(jsonPayload);
-        }
-        catch(err)
-        {
-                document.getElementById("loginResult").innerHTML = err.message;
-        }
-
+/*Setting up fonts*/
+@font-face{
+    font-family: 'adobeFont';
+    src: url('fonts/bebas.ttf') format('truetype');
 }
 
-function saveCookie()
-{
-        let minutes = 20;
-        let date = new Date();
-        date.setTime(date.getTime()+(minutes*60*1000));
-        document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
+@font-face{
+    font-family: acumin;
+    src: url('fonts/Acumin-BdPro.otf') format('opentype');
 }
 
-function readCookie()
-{
-        userId = -1;
-        let data = document.cookie;
-        let splits = data.split(",");
-        for(var i = 0; i < splits.length; i++)
-        {
-                let thisOne = splits[i].trim();
-                let tokens = thisOne.split("=");
-                if( tokens[0] == "firstName" )
-                {
-                        firstName = tokens[1];
-                }
-                else if( tokens[0] == "lastName" )
-                {
-                        lastName = tokens[1];
-                }
-                else if( tokens[0] == "userId" )
-                {
-                        userId = parseInt( tokens[1].trim() );
-                }
-        }
-
-        if( userId < 0 )
-        {
-                window.location.href = "index.html";
-        }
-        else
-        {
-                document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
-        }
+/*Background + Alignment*/
+*{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
 }
 
-function doLogout()
-{
-        userId = 0;
-        firstName = "";
-        lastName = "";
-        document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
-        window.location.href = "index.html";
+.background-video{
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    top: 0;
+    z-index: -1;
 }
 
-function addColor()
-{
-        let newColor = document.getElementById("colorText").value;
-        document.getElementById("colorAddResult").innerHTML = "";
-
-        let tmp = {color:newColor,userId,userId};
-        let jsonPayload = JSON.stringify( tmp );
-
-        let url = urlBase + '/AddColor.' + extension;
-
-        let xhr = new XMLHttpRequest();
-        xhr.open("POST", url, true);
-        xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-        try
-        {
-                xhr.onreadystatechange = function()
-                {
-                        if (this.readyState == 4 && this.status == 200)
-                        {
-                                document.getElementById("colorAddResult").innerHTML = "Color has been added";
-                        }
-                };
-                xhr.send(jsonPayload);
-        }
-        catch(err)
-        {
-                document.getElementById("colorAddResult").innerHTML = err.message;
-        }
-
+@media(min-aspect-ratio: 16/9){
+    .background-video{
+        width: 100%;
+        height: auto;
+    }
 }
-
-function searchColor()
-{
-        let srch = document.getElementById("searchText").value;
-        document.getElementById("colorSearchResult").innerHTML = "";
-
-        let colorList = "";
-
-        let tmp = {search:srch,userId:userId};
-        let jsonPayload = JSON.stringify( tmp );
-
-        let url = urlBase + '/SearchColors.' + extension;
-
-        let xhr = new XMLHttpRequest();
-        xhr.open("POST", url, true);
-        xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-        try
-        {
-                xhr.onreadystatechange = function()
-                {
-                        if (this.readyState == 4 && this.status == 200)
-                        {
-                                document.getElementById("colorSearchResult").innerHTML = "Color(s) has been retrieved";
-                                let jsonObject = JSON.parse( xhr.responseText );
-
-                                for( let i=0; i<jsonObject.results.length; i++ )
-                                {
-                                        colorList += jsonObject.results[i];
-                                        if( i < jsonObject.results.length - 1 )
-                                        {
-                                                colorList += "<br />\r\n";
-                                        }
-                                }
-
-                                document.getElementsByTagName("p")[0].innerHTML = colorList;
-                        }
-                };
-                xhr.send(jsonPayload);
-        }
-        catch(err)
-        {
-                document.getElementById("colorSearchResult").innerHTML = err.message;
-        }
-
-}
-
-
-
-function addUser() {
-    let registerLogin = document.getElementById("newUsername").value;
-    let registerPassword = document.getElementById("newPassword").value;
-    let newFirstName = document.getElementById("firstName").value;
-    let newLastName = document.getElementById("lastName").value;
-    document.getElementById("userAddResult").innerHTML = "";
-
-    let tmp = {
-        firstName: newFirstName,
-        lastName: newLastName,
-        Login: registerLogin,
-        Password: registerPassword
-    };
-    let jsonPayload = JSON.stringify(tmp);
-
-    let url = urlBase + '/AddUser.' + extension;
-
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-    try {
-        xhr.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("userAddResult").innerHTML = "User has been added";
-            }
-        };
-        xhr.send(jsonPayload);
-    } catch (err) {
-        document.getElementById("userAddResult").innerHTML = err.message;
+@media(max-aspect-ratio: 16/9){
+    .background-video{
+        width: auto;
+        height: 100%;
     }
 }
 
+.centered-square{
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 0;
+}
 
-
-//------New------//
-function addContacts() {
-    let newFirstName = document.getElementById("firstName").value;
-    let newLastName = document.getElementById("lastName").value;
-    let newEmail = document.getElementById("newEmail").value;
-    let newNumber = document.getElementById("newNumber").value;
-    document.getElementById("contactAddResult").innerHTML = "";
-
-    let tmp = {
-        firstName: newFirstName,
-        lastName: newLastName,
-        Phone: newNumber,
-        Email: newEmail
-    };
-    let jsonPayload = JSON.stringify(tmp);
-
-    let url = urlBase + '/AddContact.' + extension;
-
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-    try {
-        xhr.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("contactAddResult").innerHTML = "Contact has been added";
-            }
-        };
-        xhr.send(jsonPayload);
-    } catch (err) {
-        document.getElementById("contactAddResult").innerHTML = err.message;
-    }
+#directoryDiv {
+    font-size: 25px;
+    color: #000000;
+    width: 28%;
+    text-align: center;
+    margin-left: auto;
+    margin-right: auto;
+    opacity: 50%;
+    padding-left: 20px;
 }
 
 
-function searchContacts()
+/*Login or SignUp Popup*/
+.account-popup {
+    display: none;
+    position: fixed;
+    top: 0; 
+    left: 0; 
+    width: 100vw; 
+    height: 100vh;
+    z-index: 9;
+    background: rgba(0, 0, 0, 0.56); 
+    backdrop-filter: blur(15px);
+}
+
+.popup-container {
+    text-align: center;
+    max-width: 300px;
+    padding: 50px;
+    background: transparent;
+    border-radius: 20px;
+    position: relative;
+    top: 50%; 
+    left: 50%; 
+    transform: translate(-50%, -50%);
+}
+
+
+.popup-container input[type=text], .popup-container input[type=password], 
+.popup-container input[type=email], .popup-container input[type=email]{
+    width: 100%;
+    padding: 15px;
+    margin: 5px 0 22px 0;
+    border: none;
+    background: #f1f1f1;
+}
+
+.popup-container input[type=text]:focus, .popup-container input[type=password]:focus 
+.popup-container input[type=email]:focus, .popup-container input[type=email]:focus{
+    background-color: #ddd;
+    outline: none;
+}
+
+.popup-container .btn {
+    font-family: acumin, sans-serif;
+    font-size: 13px;
+    color:#000000;
+    letter-spacing: 5px;
+    border-radius: 0px;
+    background-color: #A9EBEC;
+
+    padding: 16px 20px;
+    border: none;
+    cursor: pointer;
+    width: 100%;
+    margin-bottom:10px;
+    opacity: 0.8;
+}
+
+.popup-container .btn:hover {
+    opacity: 50%;
+}
+
+.popup-container h1 {
+    font-family: adobeFont, sans-serif;
+    color: white;
+    align-content: center;
+    font-size: 50px;
+    opacity: 50%;
+    letter-spacing: 10px;
+}
+
+/*Other Options Little Note*/
+.sign-or-login {
+    font-family: acumin;
+    color: yellow;
+    opacity: 0.5;
+    font-size: 12px;
+    padding: 0px;
+    display: inline-block;
+    letter-spacing: 0px;
+}
+
+.sign-or-login .buttons2{
+    color: yellow;
+    font-size: 12px;
+    letter-spacing: 0px;
+    padding: 0px;
+    margin-bottom: 10px;
+    display: inline-block;
+}
+
+
+
+/*LOGGED IN*/
+#loggedInDiv
 {
-        let srch = document.getElementById("searchText").value;
-        document.getElementById("contactSearchResult").innerHTML = "";
+    font-size: 30px;
+    width: 70%;
+    margin-left: 15%;
+    padding: 25px;
+    background-color: #b2b2b2;
+}
 
-        let contactList = "";
+#accessUIDiv
+{
+    font-size: 30px;
+    width: 70%;
+    margin-left: 15%;
+    padding: 25px;
+    background-color: #b2b2b2;
+}
 
-        let tmp = {searchResults:srch,
-                   //userId:userId
-                   };
-        let jsonPayload = JSON.stringify( tmp );
+#inner-title{
+    padding-top: 105px;
+    display: inline-block;
+    font-size: 0.8em;
+    width: 90%;
+    text-align: center;
+    margin-left: 5%;
+    margin-bottom: 2%;
+    font-family: 'Ubuntu', sans-serif;
+    color: #ffffff;
+}
 
-        let url = urlBase + '/SearchContacts.' + extension;
+#signinLink {
+    display: inline-block;
+    font-size: 15px;
+    width: 90%;
+    text-align: center;
+    margin-left: 5%;
+    margin-bottom: 2%;
+    font-family: 'Ubuntu', sans-serif;
+    color: #ffffff;
+}
 
-        let xhr = new XMLHttpRequest();
-        xhr.open("POST", url, true);
-        xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-        try
-        {
-                xhr.onreadystatechange = function()
-                {
-                        if (this.readyState == 4 && this.status == 200)
-                        {
-                                document.getElementById("contactSearchResult").innerHTML = "Contact(s) has been retrieved";
-                                let jsonObject = JSON.parse( xhr.responseText );
+#loginResult{
+    display: inline-block;
+    font-size: 0.8em;
+    width: 90%;
+    text-align: center;
+    margin-left: 5%;
+    margin-bottom: 2%;
+    font-family: 'Ubuntu', sans-serif;
+    color: #95060a;
+}
 
-                                for( let i=0; i<jsonObject.results.length; i++ )
-                                {
-                                        contactList += jsonObject.results[i];
-                                        if( i < jsonObject.results.length - 1 )
-                                        {
-                                                contactList += "<br />\r\n";
-                                        }
-                                }
+#userName{
+    display: inline-block;
+    width: 80%;
+    margin-left: 10%;
+    text-align: center;
+}
 
-                                document.getElementsByTagName("p")[0].innerHTML = contactList;
-                        }
-                };
-                xhr.send(jsonPayload);
-        }
-        catch(err)
-        {
-                document.getElementById("contactSearchResult").innerHTML = err.message;
-        }
+#loginButton {
 
+    color: #009de4;
+    border-radius: 0%;
+    font-size: 25px;
+}
+
+#logoutButton{
+    width: 20%;
+    margin-left: 40%;
+}
+
+select{
+    width: 30%;
+    margin-left: 35%;
+    margin-top: 2%;
+    font-size: 0.75em;
+    font-family: 'Ubuntu', sans-serif;
+}
+
+#colorSearchResult{
+    display: none;
+}
+
+#colorAddResult{
+    display: inline-block;
+    font-size: 0.8em;
+    width: 90%;
+    text-align: center;
+    margin-left: 5%;
+    margin-top: 2%;
+    margin-bottom: 1%;
+    font-family: 'Ubuntu', sans-serif;
+    color: #95060a;
 }
 
 
 
-function goToGitHub() {
-        window.location.href = 'https://github.com/DennisKlingener/Small-Project.git';
+/*"The Reef" content*/
+.container-index{
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    width: 100%;
+    height: 100vh;
+    background-color: rgba(0,0,0,0);
+    text-align: center;
+   
+}
+.content h1{
+    font-family: adobeFont, sans-serif;
+    padding-left: 110px;
+    font-size: 220px;
+    opacity: 50%;
+    letter-spacing: 100px;
 }
 
-function checkPassword() {
-    var input = document.getElementById('psw');
-    if (input.value != document.getElementById('repsw').value) {
-        input.setCustomValidity('Password Must be Matching.');
-    } else {
-        input.setCustomValidity('');
-    }
+.content h2{
+    font-family: acumin, sans-serif;
+    padding-top: 205px;
+    padding-left: 15px;
+    font-size: 30px;
+    opacity: 50%;
 }
 
-function openLoginBox() {
-  document.getElementById("loginPopUp").style.display = "block";
-  closeSignUpBox();
+.buttons{
+    font-family: acumin, sans-serif;
+    font-size: 13px;
+    color:#A9EBEC;
+    letter-spacing: 5px;
+    border: none;
+    border-radius: 0px;
+    width: auto;
+    padding: 10px 15px;
+    margin-bottom: 5px;
+    background-color: #000000;
+    opacity: 0.8;
 }
 
-function closeLoginBox() {
-  document.getElementById("loginPopUp").style.display = "none";
+.buttons2 {
+    font-family: acumin, sans-serif;
+    font-size: 13px;
+    color: white;
+    letter-spacing: 5px;
+    border: none;
+    border-radius: 0px;
+    width: auto;
+    padding: 10px 15px;
+    background-color: transparent;
+    opacity: 0.8;
 }
 
-function openSignUpBox() {
-  document.getElementById("signUpPopUp").style.display = "block";
-  closeLoginBox();
+.buttons:hover{
+    opacity: 50%;
 }
 
-function closeSignUpBox() {
-  document.getElementById("signUpPopUp").style.display = "none";
+.buttons2:hover {
+    opacity: 50%;
+}
+
+
+
+
+
+/*------Contacts Page------*/
+#contactsBody {
+    overflow: hidden;
+}
+
+#contactsContainer {
+    background-color: transparent;
+    margin: 60px;
+    height: calc(100vh - 160px);
+    backdrop-filter: blur(15px);
+    border-radius: 30px;   
+}
+
+#leftSideDiv {
+    position: absolute;
+    background-color: black;
+    opacity: 40%;
+    width: 25%;
+    height: calc(100vh - 160px);
+    margin: 20px;
+    border-radius: 7px;
+}
+
+/*we can combine this with the "leftsidediv"*/
+.contactsList {
+    height: 200px;
+    overflow-y: auto;
+}
+
+#rightSideDiv {
+    position: absolute;
+    background-color: black;
+    max-width: calc(100vw - 160px - 25%);
+    max-height: calc(100vh - 160px);
+    margin-top: 20px;
+    margin-left: calc(25% + 20px);
+    width: calc(100vw - 160px - 25%);
+    height: calc(100vh - 160px);
+    border-radius: 7px;
+    opacity: 60%;
+}
+
+#searchBar {
+    display: block;
+    background-color: black;
+    text-align: left;
+    height: 40px;
+    width: 310px;
+    margin-top: 5%;
+    margin-left: auto;  
+    margin-right: auto;  
+    border-style: solid;
+    border-radius: 7px;
+    border-width: thin thin thin thin;
+    color: white;
+}
+
+
+#deleteButton {
+    background-color: #424245;
+    position: absolute;
+    margin-top: calc(100vh - 180px - 40px);
+    margin-left: calc(100vw - 360px - 25% + 10px);
+    border-radius: 7px;
+    opacity: 70%;
+    border: none;
+    color: white;
+}
+
+#editButton {
+    font-family: acumin, sans-serif;
+    background-color: #424245;
+    position: absolute;
+    border-radius: 7px;
+    margin-top: calc(100vh - 180px - 40px);
+    margin-left: calc(100vw - 360px - 25% - 80px);
+    opacity: 70%;
+    border: none;
+    color: white;
+}
+
+#newButton {
+    background-color: #424245;
+    position: absolute;
+    border-radius: 7px;
+    margin-top: calc(100vh - 180px - 40px);
+    margin-left: calc(100vw - 360px - 25% - 825px);
+    opacity: 70%;
+    border: none;
+    color: white;
+    top: ;
+}
+
+.contactPageButtons {
+    height: 40px;
+    width: 80px;
+}
+
+.profileIcon{
+    position: fixed;
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+    font-size: 25px;
+    color: white;
+    line-height: 80px;
+    text-align: center;
+    background-color: #424245;
+    opacity: 70%;
+    font-family: acumin;
+    margin-top: 7%;
+    margin-left: 33%;
+}
+
+.fullName{
+    position: fixed;
+    align-content: center;
+    margin-top: 15%;
+    margin-left: 31%;
+    font-size: 20px;
+    font-family: acumin;
+    text-align: center;
+    color: white;
+}
+
+.contactInformation{
+    position: fixed;
+    width: 100%;
+    padding: 15px;
+    margin: 5px 0 22px 0;
+    border: none;
+    background: #f1f1f1;
+
 }
