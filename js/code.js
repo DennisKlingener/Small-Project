c2a9efeb6df108cdc1ca54d4d660db360596f72d
willie6646 <140930952+willie6646@users.noreply.github.com>
2024-01-26 17:12:14 -0500

    Update code.js

--- js/code.js
+++ js/code.js
@@ -222,6 +222,87 @@ function addUser() {
 
 
 //------New------//
+function addContacts() {
+    let newFirstName = document.getElementById("firstName").value;
+    let newLastName = document.getElementById("lastName").value;
+    let newEmail = document.getElementById("newEmail").value;
+    let newNumber = document.getElementById("newNumber").value;
+    document.getElementById("contactAddResult").innerHTML = "";
+
+    let tmp = {
+        firstName: newFirstName,
+        lastName: newLastName,
+        Phone: newNumber,
+        Email: newEmail
+    };
+    let jsonPayload = JSON.stringify(tmp);
+
+    let url = urlBase + '/AddContact.' + extension;
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+    try {
+        xhr.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("contactAddResult").innerHTML = "Contact has been added";
+            }
+        };
+        xhr.send(jsonPayload);
+    } catch (err) {
+        document.getElementById("contactAddResult").innerHTML = err.message;
+    }
+}
+
+
+function searchContacts()
+{
+        let srch = document.getElementById("searchText").value;
+        document.getElementById("contactSearchResult").innerHTML = "";
+
+        let contactList = "";
+
+        let tmp = {searchResults:srch,
+                   //userId:userId
+                   };
+        let jsonPayload = JSON.stringify( tmp );
+
+        let url = urlBase + '/SearchContacts.' + extension;
+
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", url, true);
+        xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+        try
+        {
+                xhr.onreadystatechange = function()
+                {
+                        if (this.readyState == 4 && this.status == 200)
+                        {
+                                document.getElementById("contactSearchResult").innerHTML = "Contact(s) has been retrieved";
+                                let jsonObject = JSON.parse( xhr.responseText );
+
+                                for( let i=0; i<jsonObject.results.length; i++ )
+                                {
+                                        contactList += jsonObject.results[i];
+                                        if( i < jsonObject.results.length - 1 )
+                                        {
+                                                contactList += "<br />\r\n";
+                                        }
+                                }
+
+                                document.getElementsByTagName("p")[0].innerHTML = contactList;
+                        }
+                };
+                xhr.send(jsonPayload);
+        }
+        catch(err)
+        {
+                document.getElementById("contactSearchResult").innerHTML = err.message;
+        }
+
+}
+
+
 
 function goToGitHub() {
         window.location.href = 'https://github.com/DennisKlingener/Small-Project.git';
