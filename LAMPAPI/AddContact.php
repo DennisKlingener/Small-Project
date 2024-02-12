335c920bebb2982267d414132a5847b96b1424e7
Delali Ekpeh <delali1332@hotmail.com>
2024-02-12 18:35:43 -0500

    AddContact now returns ID of newly made contact

--- LAMPAPI/AddContact.php
+++ LAMPAPI/AddContact.php
@@ -6,47 +6,46 @@
     $Email = $inData["Email"];
     $UserID = $inData["UserID"];
 
-        // Create database connection
-        $conn = new mysqli("localhost", "TheApi", "1verygoodPassword", "COP4331");
+    // Create database connection
+    $conn = new mysqli("localhost", "TheApi", "1verygoodPassword", "COP4331");
 
-        // Check connection
-        if ($conn->connect_error)
-        {
-                returnWithError( $conn->connect_error );
-        }
-        else
-        {
-                $stmt = $conn->prepare("INSERT into Contacts (FirstName, LastName, Phone, Email,UserID) VALUES(?, ?, ?, ?,?)");
-                $stmt->bind_param("sssss", $firstName, $lastName, $Phone, $Email, $UserID);
-                if($stmt->execute())
-        {
-            echo json_encode(["message" => "New Contact Record was created"]);
-        }
-        else
-        {
+    // Check connection
+    if ($conn->connect_error)
+    {
+        returnWithError( $conn->connect_error );
+    }
+    else
+    {
+        $stmt = $conn->prepare("INSERT into Contacts (FirstName, LastName, Phone, Email, UserID) VALUES (?, ?, ?, ?, ?)");
+        $stmt->bind_param("sssss", $firstName, $lastName, $Phone, $Email, $UserID);
+
+        if ($stmt->execute()) {
+            // Get the ID of the last inserted row
+            $last_id = $conn->insert_id;
+            // Return the ID as JSON response
+            sendResultInfoAsJson(json_encode(["message" => "New Contact Record was created", "id" => $last_id]));
+        } else {
             returnWithError($conn->error);
         }
 
         $stmt->close();
         $conn->close();
+    }
 
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
 
-        }
+    function sendResultInfoAsJson($obj)
+    {
+        header('Content-type: application/json');
+        echo $obj;
+    }
 
-        function getRequestInfo()
-        {
-                return json_decode(file_get_contents('php://input'), true);
-        }
-
-        function sendResultInfoAsJson( $obj )
-        {
-                header('Content-type: application/json');
-                echo $obj;
-        }
-
-        function returnWithError( $err )
-        {
-                $retValue = '{"error":"' . $err . '"}';
-                sendResultInfoAsJson( $retValue );
-        }
+    function returnWithError($err)
+    {
+        $retValue = '{"error":"' . $err . '"}';
+        sendResultInfoAsJson($retValue);
+    }
 ?>
